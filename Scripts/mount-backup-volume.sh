f8dc4a874517e89e0d45a338bea8f3ed7420769a
Andreas Göth <a@hrhr.se>
2019-03-27 16:12:24 +0100

    Ping server before trying to mount

--- Scripts/mount-backup-volume.sh
+++ Scripts/mount-backup-volume.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+
+ping -c 1 10.0.1.16
+if [[ $? -ne 0 ]]; then
+  echo "Backup server ping failed, aborting"
+  exit 1
+fi
+
 osascript <<EOD
 tell application "Finder"
-	if not (disk "Backup" exists) then
-		mount volume "smb://backup@10.0.1.16/Backup"
-	end if
+  if not (disk "Backup" exists) then
+    mount volume "smb://backup@10.0.1.16/Backup"
+  end if
 end tell
 EOD
+
